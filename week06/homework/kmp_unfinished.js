afc6e142b8b392d951a3bdb3a040d28e5fc74221
崔武智 <cwz@ifourthwall.com>
2020-05-20 14:04:33 +0800

    week06-commit

---
+++ week06/homework/kmp_unfinished.js
@@ -0,0 +1,9 @@
+// 可选作业：使用状态机实现KMP算法。即pattern未知的字符串匹配
+// 提示：状态可生成；可以使用闭包；使用数组实现
+// 必须：时间复杂度为O(m+n)
+
+function match(pattern, string) {
+  // ?????
+}
+
+match('ababx', 'I am groot ababbbaababax')
